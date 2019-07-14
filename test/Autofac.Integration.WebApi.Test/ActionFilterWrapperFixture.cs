22aefaff533f684583548e63aa2f2c761c57ac91
alistairjevans <alistairjevans@gmail.com>
2019-07-14 16:30:42 +0100

    Fix for issue #30; do not invoke subsequent filters in OnActionExecutedAsync if response has been set by a prior filter

--- test/Autofac.Integration.WebApi.Test/ActionFilterWrapperFixture.cs
+++ test/Autofac.Integration.WebApi.Test/ActionFilterWrapperFixture.cs
@@ -102,6 +102,62 @@ namespace Autofac.Integration.WebApi.Test
             Assert.Equal("TestActionFilter2.OnActionExecutedAsync", order[3]);
         }
 
+        [Fact]
+        public async void StopsIfFilterOnExecutingSetsResponse()
+        {
+            // Issue #30.
+            // The filter behaviour if a response is set should be as follows, to
+            // mirror the functionality of filters in the normal IActionFilter implementations.
+            //
+            // If a filter sets the response:
+            // - OnActionExecutingAsync from subsequent calls should not be invoked.
+            // - Its own OnActionExecutedAsync should not be invoked.
+            // - OnActionExecutedAsync for prior filters should still be invoked.
+            var builder = new ContainerBuilder();
+            var order = new List<string>();
+
+            builder.Register(ctx => new DelegatingLogger(s => order.Add(s)))
+                .As<ILogger>()
+                .SingleInstance();
+            builder.RegisterType<TestActionFilter>()
+                .AsWebApiActionFilterFor<TestController>(c => c.Get())
+                .InstancePerRequest();
+            builder.RegisterType<TestActionFilterWithResponse>()
+                .AsWebApiActionFilterFor<TestController>(c => c.Get())
+                .InstancePerRequest();
+            builder.RegisterType<TestActionFilter2>()
+                .AsWebApiActionFilterFor<TestController>(c => c.Get())
+                .InstancePerRequest();
+            builder.RegisterType<TestActionFilter3>()
+                .AsWebApiActionFilterFor<TestController>(c => c.Get())
+                .InstancePerRequest();
+
+            var container = builder.Build();
+
+            var resolver = new AutofacWebApiDependencyResolver(container);
+            var controllerContext = CreateControllerContext(resolver);
+            var methodInfo = typeof(TestController).GetMethod("Get");
+            var actionDescriptor = CreateActionDescriptor(methodInfo);
+            var actionContext = new HttpActionContext(controllerContext, actionDescriptor);
+            var httpActionExecutedContext = new HttpActionExecutedContext(actionContext, null);
+            var metadata = new FilterMetadata
+            {
+                ControllerType = typeof(TestController),
+                FilterScope = FilterScope.Action,
+                MethodInfo = methodInfo
+            };
+            var wrapper = new ActionFilterWrapper(metadata);
+
+            await wrapper.OnActionExecutingAsync(actionContext, CancellationToken.None);
+
+            Assert.Equal("TestActionFilter3.OnActionExecutingAsync", order[0]);
+            Assert.Equal("TestActionFilter2.OnActionExecutingAsync", order[1]);
+            Assert.Equal("TestActionFilterWithResponse.OnActionExecutingAsync", order[2]);
+            Assert.Equal("TestActionFilter2.OnActionExecutedAsync", order[3]);
+            Assert.Equal("TestActionFilter3.OnActionExecutedAsync", order[4]);
+            Assert.Equal(5, order.Count);
+        }
+
         private static HttpActionDescriptor CreateActionDescriptor(MethodInfo methodInfo)
         {
             var controllerDescriptor = new HttpControllerDescriptor { ControllerType = methodInfo.DeclaringType };
