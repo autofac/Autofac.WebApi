e64808461e71acb049c95fe963228d06c4dca2ff
彭伟 <pengweiqhca@sina.com>
2021-11-18 12:58:15 +0800

    Use `Holder`

--- src/Autofac.Integration.WebApi/HttpRequestMessageProvider.cs
+++ src/Autofac.Integration.WebApi/HttpRequestMessageProvider.cs
@@ -23,6 +23,7 @@
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 // OTHER DEALINGS IN THE SOFTWARE.
 
+using System.Diagnostics.CodeAnalysis;
 using System.Net.Http;
 using System.Threading;
 
@@ -30,19 +31,37 @@ namespace Autofac.Integration.WebApi
 {
     internal static class HttpRequestMessageProvider
     {
-        private static readonly AsyncLocal<HttpRequestMessage> CurrentRequest = new AsyncLocal<HttpRequestMessage>();
+        private static readonly AsyncLocal<HttpRequestMessageHolder> CurrentRequest = new AsyncLocal<HttpRequestMessageHolder>();
 
         internal static HttpRequestMessage Current
         {
             get
             {
-                return CurrentRequest.Value;
+                return CurrentRequest.Value?.Message;
             }
 
             set
             {
-                CurrentRequest.Value = value;
+                var holder = CurrentRequest.Value;
+                if (holder != null)
+                {
+                    // Clear current HttpContext trapped in the AsyncLocals, as its done.
+                    holder.Message = null;
+                }
+
+                if (value != null)
+                {
+                    // Use an object indirection to hold the HttpContext in the AsyncLocal,
+                    // so it can be cleared in all ExecutionContexts when its cleared.
+                    CurrentRequest.Value = new HttpRequestMessageHolder { Message = value };
+                }
             }
+        }
+
+        private sealed class HttpRequestMessageHolder
+        {
+            [SuppressMessage("SA1401", "SA1401", Justification = "Field is only used during testing.")]
+            public HttpRequestMessage Message;
         }
     }
 }
